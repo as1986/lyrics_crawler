f056e86cd0b28036b9d35e528657aae16da66690
Ki-Cheng Lim <chu.cheng.lin@gmail.com>
2014-04-14 15:23:52 -0400

    cleaner

---
+++ cleaner.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+__author__ = 'as1986'
+
+
+def clean(list_of_lines):
+    to_return = []
+    for each_line in list_of_lines:
+        if each_line.startswith('['):
+            continue
+        if '歌詞' in each_line:
+            continue
+        if '演唱' in each_line:
+            continue
+        if '作曲' in each_line:
+            continue
+        if '作詞' in each_line:
+            continue
+        if '編曲' in each_line:
+            continue
+        to_return.append(each_line)
