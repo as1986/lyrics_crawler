de9a9e6e92715bede5fa8304c2eed6844e6cf075
Ki-Cheng Lim <chu.cheng.lin@gmail.com>
2014-04-14 15:41:49 -0400

    utf8

--- prepare_brown.py
+++ prepare_brown.py
@@ -5,17 +5,21 @@ __author__ = 'as1986'
 
 
 def main():
-    import json, sys,cleaner
+    import json, sys,cleaner, glob
+
 
     with open('brown_output', 'w') as w_fh:
-        for each_l in open(sys.argv[1]):
-            (title, js) = each_l.strip().split('\t')
-            list_lyrics = json.loads(js)
-            if list_lyrics is not None and len(list_lyrics) > 0:
-                lyr = cleaner.clean(list_lyrics[0])
-                for each_lyrline in lyr:
-                    to_append = ' '.join(each_lyrline.strip()).encode('utf-8')
-                    w_fh.write(to_append + '\n')
+        lyrics_files = glob.glob('*.lyrics')
+        for each_lyr in lyrics_files:
+            for each_l in open(each_lyr):
+                (title, js) = each_l.strip().split('\t')
+                list_lyrics = json.loads(js)
+                if list_lyrics is not None and len(list_lyrics) > 0:
+                    lyr = cleaner.clean(list_lyrics[0])
+                    for each_lyrline in lyr:
+                        to_append = ' '.join(each_lyrline.strip()).encode('utf-8'
+                                                                          '')
+                        w_fh.write(to_append + '\n')
 
     return
 
