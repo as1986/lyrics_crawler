08f0a9ec244644e53928cdd7983c3079ee13f2c8
Ki-Tsìng Lîm <chu.cheng.lin@gmail.com>
2014-04-14 10:48:30 -0400

    stripper

---
+++ strip_text.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+__author__ = 'as1986'
+
+
+def retrieve_file(partial_url):
+    import requests
+    from bs4 import BeautifulSoup
+
+    r = requests.get('http://mojim.com/{}'.format(partial_url))
+    soup = BeautifulSoup(r.text)
+    raw = [x for x in soup.select("#fsZ")[0].stripped_strings]
+    return raw
+
+
+def main():
+    import sys
+    import json
+
+    for l in open(sys.argv[1]):
+        (title, js) = l.strip().split('\t')
+        print 'title: {}, js: {}\n'.format(title, js)
+
+    return
+
+
+if __name__ == '__main__':
+    main()
