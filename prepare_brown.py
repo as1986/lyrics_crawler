c5dbd5bbc62f49f6f06ef0abf4c9bff92d4c9361
Ki-Cheng Lim <chu.cheng.lin@gmail.com>
2014-04-14 15:33:54 -0400

    preparing brown clustering

---
+++ prepare_brown.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+__author__ = 'as1986'
+
+
+def main():
+    import json, sys,cleaner
+
+    with open('brown_output', 'w') as w_fh:
+        for each_l in open(sys.argv[1]):
+            (title, js) = each_l.strip().split('\t')
+            list_lyrics = json.loads(js)
+            if list_lyrics is not None and len(list_lyrics) > 0:
+                lyr = cleaner.clean(list_lyrics[0])
+                for each_lyrline in lyr:
+                    to_append = ' '.join(each_lyrline.strip())
+                    w_fh.write(to_append + '\n')
+
+    return
+
+
+if __name__ == '__main__':
+    main()
